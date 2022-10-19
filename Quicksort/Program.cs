8df541bd65c9a737f720a81fad970eb29c75082a
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-10-19 16:56:45 +0700

    make text enter array elements

--- Quicksort/Program.cs
+++ Quicksort/Program.cs
@@ -28,6 +28,9 @@ namespace Quicksort
                 else
                     Console.WriteLine("\nArray can have maximum 20 elements\n");
             }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
         }
         static void Main(string[] args)
         {
