ec1af998700487377ac2551a15916ac5726db9f9
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-10-19 16:45:45 +0700

    make private int

--- Quicksort/Program.cs
+++ Quicksort/Program.cs
@@ -8,6 +8,14 @@ namespace Quicksort
 {
     class Program
     {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparison
+        private int mov_count = 0; // number of data movements
+
+        //number of element in array
+        private int n;
+
         static void Main(string[] args)
         {
         }
